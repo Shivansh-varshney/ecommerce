37c5c4da8e1274419479affa337fee4f9b095c9b
shivansh <shivanshvarshney45@gmail.com>
2025-05-01 16:37:49 +0530

    covert to function based views

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,8 @@
-from django.urls import path, include
-from .views import *
+from django.urls import path
+from .views import product_list_api_view, order_create_api_view, user_orders_api_view
 
 urlpatterns = [
-    path('products/', ProductListAPIView.as_view(), name="products"),
-    path('create_order/', OrderCreateAPIView.as_view(), name="create_order"),
-    path('user_orders/', UserOrdersAPIView.as_view(), name="user_orders")
+    path('products/', product_list_api_view, name="products"),
+    path('create_order/', order_create_api_view, name="create_order"),
+    path('user_orders/', user_orders_api_view, name="user_orders"),
 ]
--- core/views.py
+++ core/views.py
@@ -1,21 +1,26 @@
-from rest_framework import generics
-from rest_framework.permissions import IsAuthenticated
-from django.contrib.auth.models import User
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
 from .models import Product, Order
 from .serializers import ProductSerializer, OrderSerializer
 
-class ProductListAPIView(generics.ListAPIView):
-    queryset = Product.objects.all()
-    serializer_class = ProductSerializer
+@api_view(['GET'])
+def product_list_api_view(request):
+    products = Product.objects.all()
+    serializer = ProductSerializer(products, many=True)
+    return Response(serializer.data)
 
-class OrderCreateAPIView(generics.CreateAPIView):
-    queryset = Order.objects.all()
-    serializer_class = OrderSerializer
+@api_view(['POST'])
+def order_create_api_view(request):
+    serializer = OrderSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-class UserOrdersAPIView(generics.ListAPIView):
-    serializer_class = OrderSerializer
-
-    def get_queryset(self):
-        user_id = self.kwargs['id']
-        return Order.objects.filter(user_id=user_id)
+@api_view(['GET'])
+def user_orders_api_view(request, id):
+    orders = Order.objects.filter(user_id=id)
+    serializer = OrderSerializer(orders, many=True)
+    return Response(serializer.data)
